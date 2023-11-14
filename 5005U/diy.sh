94c4fd36d01e65359221078921268787a8da2d89
seanhu1010 <seanhu1010@gmail.com>
2023-11-14 23:04:21 +0800

    keep 2 app

--- 5005U/diy.sh
+++ 5005U/diy.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.1.2/g' package/base-files/files/bin/config_generate
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feeds/packages/net/v2ray-geodata
+# rm -rf feeds/luci/applications/luci-app-passwall feeds/packages/net/haproxy feeds/packages/net/v2ray-geodata
 
 # Add packages
-git clone https://github.com/nantayo/my-pkg package/my-pkg
+# git clone https://github.com/nantayo/my-pkg package/my-pkg
+
+# MosDNS
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+# kms服务器
+svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd
